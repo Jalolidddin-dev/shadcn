8f7b7222ac378073741e1208f43977c38832f88a
Dew0277 <jaloliddina98@gmail.com>
2024-08-26 03:59:44 -0700

    new project added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-700">Salom hammaga</h1>
+      <Button variant={"jaloliddin"}>Submit</Button>
+      <Button variant={"destructive"}>Register</Button>
+    </>
+  );
+}
+
+export default App;
